e97d09a1fa37476847672b7db4dce221a5fd4b60
Sergio Mendoza <smendoza787@gmail.com>
2017-10-15 00:54:51 -0700

    changed dynamic scroll js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(function() {
   $(document).scroll(function() {
     var $nav = $('.navbar')
-    $nav.toggleClass('scrolled', $(this).scrollTop() > ($('.welcome').height() - 50))
+    $nav.toggleClass('scrolled', $(this).scrollTop() > ($('.welcome').height() - 275))
   })
   $(window).scroll(function(){
     var $wScroll = $(this).scrollTop()
     $('.name').css({
-      'transform' : 'translate(0px, '+ $wScroll /2 +'%)'
+      'transform' : 'translate(0px, '+ $wScroll /2.5 +'%)'
     })
     $('.title').css({
       'opacity' : Math.floor($wScroll) + '.0'
