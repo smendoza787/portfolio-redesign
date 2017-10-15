233c72960d428c94b50d264bcd43fda72f94e99f
Sergio Mendoza <smendoza787@gmail.com>
2017-10-14 20:04:32 -0700

    inserting old portfolio data

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ $(function() {
   })
   $(window).scroll(function(){
     var $wScroll = $(this).scrollTop()
+    console.log($wScroll);
+    
     $('.name').css({
       'transform' : 'translate(0px, '+ $wScroll /2 +'%)'
+    })
+    $('.title').css({
+      'opacity' : Math.floor($wScroll) + '.0'
     })
   })
 })
